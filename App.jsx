110d277ded04bd570eb14bf92e14776dbcb19128
aoyarzabal <aoyarzabal@gmail.com>
2016-09-10 12:45:22 -0500

    initial commit

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class App extends React.Component {
+   render() {
+      
+   		var i = 1;
+
+   		return (
+   		<div>
+   			<h1>{i == 1 ? 'True!' : 'False'}</h1>
+   		</div>
+
+      );
+   }
+}
+
+export default App;
